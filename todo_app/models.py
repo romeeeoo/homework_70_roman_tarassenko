87ed4c9c597f3ada26969dc531f15560ab56ae5a
Roman Tarassenko <tarassenko@me.com>
2022-09-19 22:48:14 +0300

    Project setup
    In admin.py registered Task model.
    Added index.html.
    Described Task model in models.py.
    Configured todo_app.urls and core.urls.
    Described index_view in todo_app.views

--- todo_app/models.py
+++ todo_app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Task(models.Model):
+    STATUS_CHOICES = [
+        ('new', 'New'),
+        ('in_process', 'In Process'),
+        ('done', 'Done')
+    ]
+    description = models.CharField(max_length=200, null=False, blank=False)
+    status = models.CharField(null=False, blank=False, choices=STATUS_CHOICES, default='new', max_length=50)
+    deadline = models.DateField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return "{}. {}".format(self.pk, self.description, self.status)
--- todo_app/views.py
+++ todo_app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from todo_app.models import Task
+
 
 # Create your views here.
+def index_view(request):
+    tasks = Task.objects.all()
+    context = {'tasks': tasks}
+    return render(request, 'index.html', context)
