39d9670b7f03605116ab538e6c6e67f941ca8355
Roman Tarassenko <tarassenko@me.com>
2022-09-20 14:00:04 +0300

    Added ability to add new task
    - Add_new_task.html template described;
    - added add/ path in todo_app.urls
    - added add_view in views.py

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from todo_app.views import index_view
+from todo_app.views import index_view, add_view
 
 urlpatterns = [
-    path('', index_view)
+    path('', index_view, name="index_view"),
+    path('add/', add_view)
 ]
--- todo_app/views.py
+++ todo_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from todo_app.models import Task
 
 
@@ -7,3 +7,22 @@ def index_view(request):
     tasks = Task.objects.all()
     context = {'tasks': tasks}
     return render(request, 'index.html', context)
+
+
+def add_view(request):
+    if request.method == 'GET':
+        return render(request, 'add_new_task.html')
+    elif request.method == 'POST':
+        description = request.POST.get('description'),
+        status = request.POST.get('status'),
+        deadline = request.POST.get('deadline')
+        new_task = Task.objects.create(description=description, status=status,
+                                       deadline=deadline)
+        # new_task = Task()
+        # new_task.description = request.POST.get('description')
+        # new_task.description = request.POST.get('status')
+        # new_task.deadline = request.POST.get('deadline')
+        print(f"this is new task {new_task}")
+        print(Task.objects.all())
+        new_task.save()
+        return redirect("index_view")
