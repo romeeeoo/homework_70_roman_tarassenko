b7cc50e2409a3a0719fad7c0252fcccf0ce8d9c6
Roman Tarassenko <tarassenko@me.com>
2022-09-22 18:45:45 +0300

    Добавлен функционал добавления нового задания. Все еще нужна отладка отправки формы с пустой датой.

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -3,7 +3,7 @@ from todo_app.views import index_view, add_view, detail_view
 
 urlpatterns = [
     path('', index_view, name='index'),
-    path('add/', add_view),
+    path('add/', add_view, name='add_task'),
     path('task/<int:pk>', detail_view, name='detailed_task'),
     path('task/', index_view)
 ]
--- todo_app/views.py
+++ todo_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect, get_object_or_404
+
 from todo_app.models import Task
 
 
@@ -13,17 +14,15 @@ def add_view(request):
     if request.method == 'GET':
         return render(request, 'add_new_task.html')
     elif request.method == 'POST':
-        description = request.POST.get('description'),
-        status = request.POST.get('status'),
-        deadline = request.POST.get('deadline')
-        new_task = Task.objects.create(description=description[0], status=status[0],
-                                       deadline=deadline)
-        new_task.save()
-        return redirect("index_view")
+        task_data = {'description': request.POST.get('description'),
+                     'status': request.POST.get('status'),
+                     # 'deadline': request.POST.get('deadline'),
+                     'detailed_description': request.POST.get('detailed_description')
+                     }
+        new_task = Task.objects.create(**task_data)
+        return redirect("detailed_task", pk=new_task.pk)
 
 
 def detail_view(request, pk):
     task = get_object_or_404(Task, pk=pk)
     return render(request, 'detailed_task.html', context={'task': task})
-
-
