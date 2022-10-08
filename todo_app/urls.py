c915329a0c50afe84e09a52918cc58f2836eb6ce
Roman Tarassenko <tarassenko@me.com>
2022-10-08 18:17:20 +0300

    Создана страница для редактирования задачи на базе классового представления UpdateTaskView

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
-from todo_app.views import update_view, delete_view, confirm_delete_view, IndexView, TaskView, AddTaskView
+from todo_app.views import delete_view, confirm_delete_view, IndexView, TaskView, AddTaskView, UpdateTaskView
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
     path('add/', AddTaskView.as_view(), name='add_task'),
     path('task/<int:pk>/', TaskView.as_view(), name='detailed_task'),
     path('task/', IndexView.as_view(), name='task_no_pk'),
-    path('task/<int:pk>/update/', update_view, name='update_task'),
+    path('task/<int:pk>/update/', UpdateTaskView.as_view(), name='update_task'),
     path('task/<int:pk>/delete', delete_view, name='delete_task'),
     path('task/<int:pk>/confirm-delete', confirm_delete_view, name='confirm_delete_task')
 ]
--- todo_app/views.py
+++ todo_app/views.py
@@ -16,23 +16,6 @@ class IndexView(TemplateView):
         return context
 
 
-# def add_view(request):
-#     if request.method == 'GET':
-#         form = TaskForm()
-#         return render(request, 'add_new_task.html', context={'form': form})
-#     elif request.method == 'POST':
-#         form = TaskForm(request.POST)
-#         if form.is_valid():
-#             new_task = Task.objects.create(
-#                 description=form.cleaned_data['description'],
-#                 detailed_description=form.cleaned_data['detailed_description'],
-#                 status=form.cleaned_data['status'],
-#                 deadline=form.cleaned_data['deadline']
-#             )
-#             return redirect("detailed_task", pk=new_task.pk)
-#         else:
-#             return render(request, 'add_new_task.html', context={'form': form})
-
 class AddTaskView(TemplateView):
     template_name = "add_new_task.html"
 
@@ -59,27 +42,47 @@ class TaskView(TemplateView):
         print(context)
         return context
 
+# def update_view(request, pk):
+#     task = get_object_or_404(Task, pk=pk)
+#     if request.method == 'POST':
+#         form = TaskForm(request.POST)
+#         if form.is_valid():
+#             task.description = form.cleaned_data['description']
+#             task.detailed_description = form.cleaned_data['detailed_description']
+#             task.status = form.cleaned_data['status']
+#             task.deadline = form.cleaned_data['deadline']
+#             task.save()
+#             return redirect('detailed_task', pk=task.pk)
+#     elif request.method == 'GET':
+#         form = TaskForm(initial={
+#             'description': task.description,
+#             'detailed_description': task.detailed_description,
+#             'status': task.status,
+#             'deadline': task.deadline
+#         })
+#         return render(request, 'update_task.html', context={'task': task, 'form': form})
 
 
-def update_view(request, pk):
-    task = get_object_or_404(Task, pk=pk)
-    if request.method == 'POST':
+class UpdateTaskView(TemplateView):
+    template_name = "update_task.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["task"] = get_object_or_404(Task, pk=kwargs["pk"])
+        return context
+
+    def get(self, request, *args, **kwargs):
+        context = self.get_context_data(**kwargs)
+        form = TaskForm(instance=context["task"])
+        context["form"] = form
+        return self.render_to_response(context)
+
+    def post(self, request, *args, **kwargs):
         form = TaskForm(request.POST)
         if form.is_valid():
-            task.description = form.cleaned_data['description']
-            task.detailed_description = form.cleaned_data['detailed_description']
-            task.status = form.cleaned_data['status']
-            task.deadline = form.cleaned_data['deadline']
-            task.save()
-            return redirect('detailed_task', pk=task.pk)
-    elif request.method == 'GET':
-        form = TaskForm(initial={
-            'description': task.description,
-            'detailed_description': task.detailed_description,
-            'status': task.status,
-            'deadline': task.deadline
-        })
-        return render(request, 'update_task.html', context={'task': task, 'form': form})
+            task = form.save()
+            return redirect("detailed_task", pk=task.pk)
+        return render(request, "add_new_task.html", context={"form": form})
 
 
 def delete_view(request, pk):
